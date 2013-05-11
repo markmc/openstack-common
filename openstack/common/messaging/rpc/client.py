openstack/oslo.messaging

Initial framework

openstack/common/messaging/rpc/client.py
@@ -1,0 +1,118 @@
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import inspect
+
+from oslo.config import cfg
+
+from openstack.common.gettextutils import _
+from openstack.common import local
+from openstack.common import log as logging
+from openstack.common.messaging import target
+
+_client_opts = [
+    cfg.IntOpt('rpc_response_timeout',
+               default=60,
+               help='Seconds to wait for a response from a call'),
+]
+
+_LOG = logging.getLogger(__name__)
+
+
+class _CallContext(object):
+
+    def __init__(self, transport, target, timeout=None, check_for_lock=None):
+        self.conf = transport.conf
+
+        self.transport = transport
+        self.target = target
+        self.timeout = timeout
+        self.check_for_lock = check_for_lock
+
+        super(CallContext, self).__init__()
+
+    def _make_message(self, method, args):
+        msg = dict(method=method, args=args)
+        if self.target.namespace is not None:
+            msg['namespace'] = self.target.namespace
+        if self.target.version is not None:
+            msg['version'] = self.target.version
+        return msg
+
+    def cast(self, method, **kwargs):
+        msg = self._make_message(method, kwargs)
+        self.transport._send(target, msg)
+
+    def _check_for_lock():
+        if not self.conf.debug:
+            return None
+
+        if (hasattr(local.strong_store, 'locks_held') and
+            local.strong_store.locks_held):
+            stack = ' :: '.join([frame[3] for frame in inspect.stack()])
+            _LOG.warn(_('An RPC is being made while holding a lock. The locks '
+                        'currently held are %(locks)s. This is probably a bug. '
+                        'Please report it. Include the following: [%(stack)s].'),
+                      {'locks': local.strong_store.locks_held, 'stack': stack})
+            return True
+
+        return False
+
+    def call(self, method, **kwargs):
+        msg = self._make_message(method, kwargs)
+
+        timeout = self.timeout
+        if self.timeout is None:
+            self.conf.rpc_response_timeout
+
+        if self.check_for_lock:
+            self._check_for_lock()
+
+        return self.transport._send(target, msg,
+                                    wait_for_reply=True, timeout=timeout)
+
+
+class RPCClient(object):
+
+    def __init__(self, transport, target, timeout=None, check_for_lock=None):
+        self.conf = transport.conf
+        self.conf.register_opts(_client_opts)
+
+        self.transport = transport
+        self.target = target
+        self.timeout = timeout
+        self.check_for_lock = check_for_lock
+
+        super(RPCClient, self).__init__()
+
+    def prepare(self, host=None, version=None,
+                timeout=None, check_for_lock=None):
+        target = self.target(host=host, version=version)
+
+        if timeout is None:
+            timeout = self.timeout
+        if check_for_lock is None:
+            check_for_lock = self.check_for_lock
+
+        return _CallContext(self.transport, target, timeout, check_for_lock)
+
+    def cast(self, method, **kwargs):
+        self.prepare().cast(method, **kwargs)
+
+    def call(self, method, **kwargs):
+        return self.prepare().call(method, **kwargs)

openstack/common/messaging/target.py
@@ -1,0 +1,67 @@
+
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class Target(object):
+
+    """Identifies the destination of messages.
+
+    A Target encapsulates all the information to identify where a message
+    should be sent or what messages a server is listening for.
+
+    Its attributes are:
+
+    :param exchange: A scope for topics. Leave unspecified to default to the
+      control_exchange configuration option.
+    :type exchange: str
+    :param topic: A name which identifies the set of interfaces exposed by a
+      server. Multiple servers may listen on a topic and messages will be
+      dispatched to one of the servers in a round-robin fashion.
+    :type topic: str
+    :param namespace: Identifies a particular interface (i.e. set of methods)
+      exposed by a server. The default interface has no namespace identifier
+      and is referred to as the null namespace.
+    :type topic: str
+    :param version: Interfaces have a major.minor version number associated
+      with them. A minor number increment indicates a backwards compatible
+      change and an incompatible change is indicated by a major number bump.
+      Servers may implement multiple major versions and clients may require
+      indicate that their message requires a particular minimum minor version.
+    :type topic: str
+    :param server: Clients can request that a message be directed to a specific
+      server, rather than just one of a pool of servers listening on the topic.
+    :type topic: str
+    :param fanout: Clients may request that a message be directed to all
+      servers listening on a topic by setting fanout to ``True``, rather than
+      just one of them.
+    :type topic: bool
+    """
+
+    def __init__(self, exchange=None, topic=None, namespace=None,
+                 version=None, server=None, fanout=None):
+        self.exchange = exchange
+        self.topic = topic
+        self.namespace = namespace
+        self.version = version
+        self.server = server
+        self.fanout = fanout
+
+    def __call__(self, **kwargs):
+        kwargs.setdefault('exchange', self.exchange)
+        kwargs.setdefault('topic', self.topic)
+        kwargs.setdefault('namespace', self.version)
+        kwargs.setdefault('server', self.server)
+        kwargs.setdefault('fanout', self.fanout)
+        return Target(**kwargs)

openstack/common/messaging/transport.py
@@ -1,0 +1,82 @@
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import urlparse
+
+from oslo.config import cfg
+from stevedore import driver
+
+from openstack.common.messaging import drivers
+
+_transport_opts = [
+    cfg.StrOpt('transport_url',
+               default=None,
+               help='A URL representing the messaging driver to use and its '
+                    'full configuration. If not set, we fall back to the '
+                    'rpc_backend option and driver specific configuration.'),
+    cfg.StrOpt('rpc_backend',
+               default='kombu',
+               help='The messaging driver to use, defaults to kombu. Other '
+                    'drivers include qpid and zmq.'),
+    cfg.StrOpt('control_exchange',
+               default='openstack',
+               help='The default exchange under which topics are scoped. May '
+                    'be overridden by an exchange name specified in the '
+                    'transport_url option.'),
+]
+
+
+def set_defaults(control_exchange):
+    cfg.set_defaults(_transport_opts,
+                     control_exchange=control_exchange)
+
+
+class Transport(object):
+
+    def __init__(self, driver):
+        self.conf = driver.conf
+        self._driver = driver
+
+    def send(self, target, message, wait_for_reply=None, timeout=None):
+        return self._driver._send(target, message,
+                                  wait_for_reply=wait_for_reply,
+                                  timeout=timeout)
+
+    def listen(self, target):
+        return self._driver._listen(target)
+
+
+def get_transport(conf, url=None):
+    conf.register_opts(_transport_opts)
+
+    url = url or conf.transport_url
+    if url is not None:
+        rpc_backend = urlparse.urlparse(url).scheme
+    else:
+        rpc_backend = conf.rpc_backend
+
+    kwargs = dict(default_exchange=conf.control_exchange)
+    if url is not None:
+        kwargs['url'] = url
+
+    mgr = driver.DriverManager(drivers.NAMESPACE,
+                               rpc_backend,
+                               invoke_on_load=True,
+                               invoke_args=[conf],
+                               invoke_kwds=kwargs)
+    return Transport(mgr.driver)

markmc/openstack-common

Change-Id: I183a6fd41a1971158f237bc94666818fcb136765
Co-authored-by: Doug Hellmann <doug.hellmann@dreamhost.com>
Co-authored-by: Flaper Fesp <flaper87@gmail.com>

openstack/common/messaging/rpc/client.py
@@ -1,0 +1,118 @@
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import inspect
+
+from oslo.config import cfg
+
+from openstack.common.gettextutils import _
+from openstack.common import local
+from openstack.common import log as logging
+from openstack.common.messaging import target
+
+_client_opts = [
+    cfg.IntOpt('rpc_response_timeout',
+               default=60,
+               help='Seconds to wait for a response from a call'),
+]
+
+_LOG = logging.getLogger(__name__)
+
+
+class _CallContext(object):
+
+    def __init__(self, transport, target, timeout=None, check_for_lock=None):
+        self.conf = transport.conf
+
+        self.transport = transport
+        self.target = target
+        self.timeout = timeout
+        self.check_for_lock = check_for_lock
+
+        super(_CallContext, self).__init__()
+
+    def _make_message(self, method, args):
+        msg = dict(method=method, args=args)
+        if self.target.namespace is not None:
+            msg['namespace'] = self.target.namespace
+        if self.target.version is not None:
+            msg['version'] = self.target.version
+        return msg
+
+    def cast(self, method, **kwargs):
+        msg = self._make_message(method, kwargs)
+        self.transport._send(target, msg)
+
+    def _check_for_lock():
+        if not self.conf.debug:
+            return None
+
+        if (hasattr(local.strong_store, 'locks_held') and
+            local.strong_store.locks_held):
+            stack = ' :: '.join([frame[3] for frame in inspect.stack()])
+            _LOG.warn(_('An RPC is being made while holding a lock. The locks '
+                        'currently held are %(locks)s. This is probably a bug. '
+                        'Please report it. Include the following: [%(stack)s].'),
+                      {'locks': local.strong_store.locks_held, 'stack': stack})
+            return True
+
+        return False
+
+    def call(self, method, **kwargs):
+        msg = self._make_message(method, kwargs)
+
+        timeout = self.timeout
+        if self.timeout is None:
+            timeout = self.conf.rpc_response_timeout
+
+        if self.check_for_lock:
+            self._check_for_lock()
+
+        return self.transport._send(self.target, msg,
+                                    wait_for_reply=True, timeout=timeout)
+
+
+class RPCClient(object):
+
+    def __init__(self, transport, target, timeout=None, check_for_lock=None):
+        self.conf = transport.conf
+        self.conf.register_opts(_client_opts)
+
+        self.transport = transport
+        self.target = target
+        self.timeout = timeout
+        self.check_for_lock = check_for_lock
+
+        super(RPCClient, self).__init__()
+
+    def prepare(self, server=None, version=None,
+                timeout=None, check_for_lock=None):
+        target = self.target(server=server, version=version)
+
+        if timeout is None:
+            timeout = self.timeout
+        if check_for_lock is None:
+            check_for_lock = self.check_for_lock
+
+        return _CallContext(self.transport, target, timeout, check_for_lock)
+
+    def cast(self, method, **kwargs):
+        self.prepare().cast(method, **kwargs)
+
+    def call(self, method, **kwargs):
+        return self.prepare().call(method, **kwargs)

openstack/common/messaging/target.py
@@ -1,0 +1,68 @@
+
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class Target(object):
+
+    """Identifies the destination of messages.
+
+    A Target encapsulates all the information to identify where a message
+    should be sent or what messages a server is listening for.
+
+    Its attributes are:
+
+    :param exchange: A scope for topics. Leave unspecified to default to the
+      control_exchange configuration option.
+    :type exchange: str
+    :param topic: A name which identifies the set of interfaces exposed by a
+      server. Multiple servers may listen on a topic and messages will be
+      dispatched to one of the servers in a round-robin fashion.
+    :type topic: str
+    :param namespace: Identifies a particular interface (i.e. set of methods)
+      exposed by a server. The default interface has no namespace identifier
+      and is referred to as the null namespace.
+    :type topic: str
+    :param version: Interfaces have a major.minor version number associated
+      with them. A minor number increment indicates a backwards compatible
+      change and an incompatible change is indicated by a major number bump.
+      Servers may implement multiple major versions and clients may require
+      indicate that their message requires a particular minimum minor version.
+    :type topic: str
+    :param server: Clients can request that a message be directed to a specific
+      server, rather than just one of a pool of servers listening on the topic.
+    :type topic: str
+    :param fanout: Clients may request that a message be directed to all
+      servers listening on a topic by setting fanout to ``True``, rather than
+      just one of them.
+    :type topic: bool
+    """
+
+    def __init__(self, exchange=None, topic=None, namespace=None,
+                 version=None, server=None, fanout=None):
+        self.exchange = exchange
+        self.topic = topic
+        self.namespace = namespace
+        self.version = version
+        self.server = server
+        self.fanout = fanout
+
+    def __call__(self, **kwargs):
+        kwargs.setdefault('exchange', self.exchange)
+        kwargs.setdefault('topic', self.topic)
+        kwargs.setdefault('namespace', self.namespace)
+        kwargs.setdefault('version', self.version)
+        kwargs.setdefault('server', self.server)
+        kwargs.setdefault('fanout', self.fanout)
+        return Target(**kwargs)

openstack/common/messaging/transport.py
@@ -1,0 +1,82 @@
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import urlparse
+
+from oslo.config import cfg
+from stevedore import driver
+
+from openstack.common.messaging import drivers
+
+_transport_opts = [
+    cfg.StrOpt('transport_url',
+               default=None,
+               help='A URL representing the messaging driver to use and its '
+                    'full configuration. If not set, we fall back to the '
+                    'rpc_backend option and driver specific configuration.'),
+    cfg.StrOpt('rpc_backend',
+               default='kombu',
+               help='The messaging driver to use, defaults to kombu. Other '
+                    'drivers include qpid and zmq.'),
+    cfg.StrOpt('control_exchange',
+               default='openstack',
+               help='The default exchange under which topics are scoped. May '
+                    'be overridden by an exchange name specified in the '
+                    'transport_url option.'),
+]
+
+
+def set_defaults(control_exchange):
+    cfg.set_defaults(_transport_opts,
+                     control_exchange=control_exchange)
+
+
+class Transport(object):
+
+    def __init__(self, driver):
+        self.conf = driver.conf
+        self._driver = driver
+
+    def _send(self, target, message, wait_for_reply=None, timeout=None):
+        return self._driver.send(target, message,
+                                 wait_for_reply=wait_for_reply,
+                                 timeout=timeout)
+
+    def _listen(self, target):
+        return self._driver.listen(target)
+
+
+def get_transport(conf, url=None):
+    conf.register_opts(_transport_opts)
+
+    url = url or conf.transport_url
+    if url is not None:
+        rpc_backend = urlparse.urlparse(url).scheme
+    else:
+        rpc_backend = conf.rpc_backend
+
+    kwargs = dict(default_exchange=conf.control_exchange)
+    if url is not None:
+        kwargs['url'] = url
+
+    mgr = driver.DriverManager(drivers.NAMESPACE,
+                               rpc_backend,
+                               invoke_on_load=True,
+                               invoke_args=[conf],
+                               invoke_kwds=kwargs)
+    return Transport(mgr.driver)