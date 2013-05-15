6f70899d629f9f9129bb8438b8907952d37957c2
Russell Bryant <rbryant@redhat.com>
2013-05-15 16:39:04 -0400

    Support capping message versions in the client.
    When doing a rolling upgrade, we need to be able to tell all rpc clients
    to hold off on sending newer versions of messages until all nodes
    understand the new message version.  This patch adds the oslo component
    of this.
    
    It's quite simple.  The rpc proxy just stores the version cap and will
    raise an exception if code ever tries to send a message that exceeds
    this cap.
    
    Allowing the cap to be configured and generating different types of
    messages based on the configured value is the hard part here, but that
    is left up to the project using the rpc library.
    
    Implements blueprint rpc-version-control.
    
    Change-Id: Ia69db03a80dc3b1c63d52c7e163ea3cfec80c882

--- openstack/common/messaging/rpc/client.py
+++ openstack/common/messaging/rpc/client.py
@@ -23,6 +23,7 @@ from oslo.config import cfg
 from openstack.common import local
 from openstack.common import log as logging
 from openstack.common.messaging import target
+from openstack.common.messaging import _utils as utils
 
 _client_opts = [
     cfg.IntOpt('rpc_response_timeout',
@@ -33,15 +34,29 @@ _client_opts = [
 _LOG = logging.getLogger(__name__)
 
 
+class RpcVersionCapError(Exception):
+
+    def __init__(self, version, version_cap):
+        self.version = version
+        self.version_cap = version_cap
+
+    def __str__(self):
+        return ("Specified RPC version cap, %(version_cap)s, is too low. "
+                "Needs to be higher than %(version)s." %
+                dict(version=self.version, version_cap=self.version_cap))
+
+
 class _CallContext(object):
 
-    def __init__(self, transport, target, timeout=None, check_for_lock=None):
+    def __init__(self, transport, target,
+                 timeout=None, check_for_lock=None, version_cap=None):
         self.conf = transport.conf
 
         self.transport = transport
         self.target = target
         self.timeout = timeout
         self.check_for_lock = check_for_lock
+        self.version_cap = version_cap
 
         super(_CallContext, self).__init__()
 
@@ -70,6 +85,11 @@ class _CallContext(object):
                       'Please report it. Include the following: [%(stack)s].',
                       {'locks': local.strong_store.locks_held, 'stack': stack})
 
+    def _check_version_cap(self, version):
+        if not utils.version_is_compatible(self.version_cap, version):
+            raise RpcVersionCapError(version=version,
+                                     version_cap=self.version_cap)
+
     def call(self, ctxt, method, **kwargs):
         """Invoke a method and wait for a reply. See RPCClient.call()."""
         msg = self._make_message(method, kwargs)
@@ -80,6 +100,8 @@ class _CallContext(object):
 
         if self.check_for_lock:
             self._check_for_lock()
+        if self.version_cap:
+            self._check_version_cap(msg.get('version'))
 
         return self.transport._send(self.target, ctxt, msg,
                                     wait_for_reply=True, timeout=timeout)
@@ -141,7 +163,8 @@ class RPCClient(object):
     class will usually help to make the code much more obvious.
     """
 
-    def __init__(self, transport, target, timeout=None, check_for_lock=None):
+    def __init__(self, transport, target,
+                 timeout=None, check_for_lock=None, version_cap=None):
         """Construct an RPC client.
 
         :param transport: a messaging transport handle
@@ -152,6 +175,8 @@ class RPCClient(object):
         :type timeout: int or float
         :param check_for_lock: warn if a lockutils.synchronized lock is held
         :type check_for_lock: bool
+        :param version_cap: raise a RpcVersionCapError version exceeds this cap
+        :type version_cap: str
         """
         self.conf = transport.conf
         self.conf.register_opts(_client_opts)
@@ -160,6 +185,7 @@ class RPCClient(object):
         self.target = target
         self.timeout = timeout
         self.check_for_lock = check_for_lock
+        self.version_cap = version_cap
 
         super(RPCClient, self).__init__()
 
@@ -167,7 +193,7 @@ class RPCClient(object):
 
     def prepare(self, exchange=_marker, topic=_marker, namespace=_marker,
                 version=_marker, server=_marker, fanout=_marker,
-                timeout=_marker, check_for_lock=_marker):
+                timeout=_marker, check_for_lock=_marker, version_cap=_marker):
         """Prepare a method invocation context.
 
         Use this method to override client properties for an individual method
@@ -193,6 +219,8 @@ class RPCClient(object):
         :type timeout: int or float
         :param check_for_lock: warn if a lockutils.synchronized lock is held
         :type check_for_lock: bool
+        :param version_cap: raise a RpcVersionCapError version exceeds this cap
+        :type version_cap: str
         """
         kwargs = dict(
             exchange=exchange,
@@ -209,8 +237,11 @@ class RPCClient(object):
             timeout = self.timeout
         if check_for_lock is self._marker:
             check_for_lock = self.check_for_lock
+        if version_cap is self._marker:
+            version_cap = self.version_cap
 
-        return _CallContext(self.transport, target, timeout, check_for_lock)
+        return _CallContext(self.transport, target,
+                            timeout, check_for_lock, version_cap)
 
     def cast(self, ctxt, method, **kwargs):
         """Invoke a method and return immediately.
