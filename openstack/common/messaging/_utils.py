openstack/oslo.messaging

Implement a fake driver
A bunch of FIXMEs in here, but it seems like a good start.

Note that this differs from the original fake driver. Rather than the
driver consuming each message in a greenthread, we leave it up to the
choice of executor to determine how the consumer is run in parallel to
the client and we use thread-safe queues to pass the messages and
replies back and forth. The main reason for this is that we don't want
the driver explicitly depending on eventlet.

openstack/common/messaging/_utils.py
@@ -16,6 +16,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import urlparse
+
 
 def version_is_compatible(imp_version, version):
     """Determine whether versions are compatible.
@@ -30,3 +32,29 @@ def version_is_compatible(imp_version, version):
     if int(version_parts[1]) > int(imp_version_parts[1]):  # Minor
         return False
     return True
+
+
+def exchange_from_url(self, url, default_exchange=None):
+    """Parse an exchange name from a URL.
+
+    Assuming a URL takes the form of:
+
+      transport:///myexchange
+
+    then parse the URL and return the exchange name.
+
+    :param url: the URL to parse
+    :type url: str
+    :param default_exchange: what to return if no exchange found in URL
+    :type default_exchange: str
+    """
+    if not url:
+        return default_exchange
+
+    url = urlparse.urlparse(url)
+    if not url.path.startswith('/'):
+        return default_exchange
+
+    parts = u.path[1:].split('/')
+
+    return parts[0] if parts[0] else default_exchange

markmc/openstack-common

Change-Id: Id52e12be93c95b779695f62411f02f43d8480e4e

openstack/common/messaging/_utils.py
@@ -16,6 +16,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import urlparse
+
 
 def version_is_compatible(imp_version, version):
     """Determine whether versions are compatible.
@@ -30,3 +32,29 @@ def version_is_compatible(imp_version, version):
     if int(version_parts[1]) > int(imp_version_parts[1]):  # Minor
         return False
     return True
+
+
+def exchange_from_url(self, url, default_exchange=None):
+    """Parse an exchange name from a URL.
+
+    Assuming a URL takes the form of:
+
+      transport:///myexchange
+
+    then parse the URL and return the exchange name.
+
+    :param url: the URL to parse
+    :type url: str
+    :param default_exchange: what to return if no exchange found in URL
+    :type default_exchange: str
+    """
+    if not url:
+        return default_exchange
+
+    url = urlparse.urlparse(url)
+    if not url.path.startswith('/'):
+        return default_exchange
+
+    parts = url.path[1:].split('/')
+
+    return parts[0] if parts[0] else default_exchange